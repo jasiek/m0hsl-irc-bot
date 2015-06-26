24bbc828cfebd470914c6b880d2c755b3c07404c
Jan Szumiec <jan.szumiec@gmail.com>
2015-06-26 22:57:44 +0100

    Bot now works.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source "https://rubygems.org"
 
 gem 'cinch'
 gem 'cinch-identify'
+gem 'cinch-pskreporter'
+
+
--- bot.rb
+++ bot.rb
@@ -3,14 +3,20 @@ Bundler.require
 
 require "cinch"
 require "cinch/plugins/identify"
+require "cinch/plugins/pskreporter"
 
 bot = Cinch::Bot.new do
   configure do |c|
-    c.plugins.plugins = [Cinch::Plugins::Identify]
+    c.server = "irc.freenode.net"
+    c.channels = [ "#lhs-radio" ]
+    c.plugins.plugins = [Cinch::Plugins::Identify, Cinch::Plugins::PSKReporter]
     c.plugins.options[Cinch::Plugins::Identify] = {
-      :username => ENV['CINCH_USERNAME']
-      :password => ENV['CINCH_PASSWORD']
-      :type     => :nickserv,
+      username: ENV['CINCH_USERNAME'],
+      password: ENV['CINCH_PASSWORD'],
+      type: :nickserv,
+    }
+    c.plugins.options[Cinch::Plugins::PSKReporter] = {
+      watchers: { '#lhs-radio' => ['m0hsl'] }
     }
   end
 end
