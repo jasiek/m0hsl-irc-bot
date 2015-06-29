7ff3f357ebaada571659928e8421acd3fecbaa0c
Jan Szumiec <jan.szumiec@gmail.com>
2015-06-29 23:30:22 +0100

    Twitter updates.

--- Gemfile
+++ Gemfile
@@ -4,7 +4,4 @@ gem 'cinch'
 gem 'cinch-identify'
 gem 'cinch-pskreporter', git: 'https://github.com/jasiek/cinch-pskreporter.git', branch: 'master'
 gem 'cinch-logger-canonical'
-
-
-
-
+gem 'cinch-twitterwatch'
--- bot.rb
+++ bot.rb
@@ -9,7 +9,9 @@ bot = Cinch::Bot.new do
   configure do |c|
     c.server = "irc.freenode.net"
     c.channels = [ "#lhs-radio" ]
-    c.plugins.plugins = [Cinch::Plugins::Identify, Cinch::Plugins::PSKReporter]
+    c.plugins.plugins = [Cinch::Plugins::Identify,
+                         Cinch::Plugins::PSKReporter,
+                         Cinch::Plugins::TwitterWatch]
     c.plugins.options[Cinch::Plugins::Identify] = {
       username: ENV['CINCH_USERNAME'],
       password: ENV['CINCH_PASSWORD'],
@@ -18,7 +20,21 @@ bot = Cinch::Bot.new do
     c.plugins.options[Cinch::Plugins::PSKReporter] = {
       watchers: { '#lhs-radio' => ['m0hsl'] }
     }
+    c.plugins.options[Cinch::Plugins::TwitterWatch] = {
+      consumer_key:    ENV['TWITTER_CONSUMER_KEY'],
+      consumer_secret: ENV['TWITTER_CONSUMER_SECRET'],
+      access_token:    ENV['TWITTER_ACCESS_TOKEN'],
+      access_secret:   ENV['TWITTER_ACCESS_SECRET'],
+      watchers: {
+        '#lhs-radio' => ['m0hsl']
+      }
+    }
   end
 end
+
+bot.loggers << Cinch::Logger::CanonicalLogger.new('#lhs-radio', bot)
+
+# We tweet only from time to time.
+Cinch::Plugins::TwitterWatch.timers.last.interval = 600
 
 bot.start
